4bfccbc131ef681b173f12f5cfa8ac50cf8f4541
potaesm <suthinan.musitmani@gmail.com>
2024-06-01 16:13:02 +0700

    add limit delete

--- helpers/fileHandler.js
+++ helpers/fileHandler.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const rimraf = require("rimraf");
 const du = require("du");
+const findRemoveSync = require("find-remove");
 
 const FileHandler = class {
   createDirIfNotExists(folderPath) {
@@ -15,6 +16,14 @@ const FileHandler = class {
 
   removeDirectory(folderPath, callback) {
     rimraf(folderPath, callback);
+  }
+
+  removeOlder(folderPath, ageSeconds, extensions = [".mp4"], limit = 100) {
+    return findRemoveSync(folderPath, {
+      age: { seconds: ageSeconds },
+      extensions,
+      limit,
+    });
   }
 
   getDirectorySize(folderPath, callback) {
--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 const Recorder = require("./helpers/recorder");
-// const FileHandler = require("./helpers/fileHandler");
+const FileHandler = require("./helpers/fileHandler");
 
 const { CAM_USERNAME, CAM_PASSWORD, CAM_IP } = process.env;
 
-let rec = new Recorder({
+const rec = new Recorder({
   url: `rtsp://${CAM_USERNAME}:${CAM_PASSWORD}@${CAM_IP}:554/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif`,
-  timeLimit: 30,
+  timeLimit: 60,
   name: "front-01",
+  // https://momentjscom.readthedocs.io/en/latest/moment/04-displaying/01-format/
+  directoryPathFormat: "YYYY-MM-DD",
+  fileNameFormat: "HH-mm-ss",
 });
 
 rec.startRecording();
 
-// setTimeout(() => {
-//   console.log("Stopping Recording");
-//   rec.stopRecording();
-//   rec = null;
-// }, 30000);
+const fh = new FileHandler();
+
+setInterval(() => {
+  // console.log("Stopping Recording");
+  // rec.stopRecording();
+  fh.getDirectorySize("media", (err, byte) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const limitByteSize = 15 * 1024 * 1024 * 1024;
+    const ageOfToBeDeletedItemsInSeconds = 1 * 24 * 60 * 60;
+    if (byte >= limitByteSize) {
+      console.log("Media size has reached limit");
+      const result = fh.removeOlder("media", ageOfToBeDeletedItemsInSeconds);
+      console.log("Deleted", result);
+    } else {
+      console.log("Media size is within limit");
+    }
+  });
+}, 30000);
